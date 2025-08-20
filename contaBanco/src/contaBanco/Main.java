8565ba6be4e9c266f01d1698162dfa0a1a43d2c9
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2025-08-19 22:55:07 -0300

    project done

--- contaBanco/src/contaBanco/Main.java
+++ contaBanco/src/contaBanco/Main.java
@@ -4,19 +4,7 @@ public class Main {
 	static ContaTerminal conta=new ContaTerminal();
 	//---
 	public static void main(String[] args) {
-		conta.promptInicial();
-		//---
-		conta.inputNumeroDaConta();
-		//---
-		conta.inputAgencia();
-		//---
-		conta.inputNome();
-		//---
-		conta.inputSaldo();
-		//---
-		conta.mensagemFinal();
-		//---
-		conta.closeScanner();
+		
 	}
 
 }
